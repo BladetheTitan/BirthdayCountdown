f29dd86b01e6988a8fb5c366d86d614113a11a1a
Evan Norland <bladethetitan@gmail.com>
2018-07-18 15:51:02 +0000

    functional button component and styles

---
+++ src/components/button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button className="button">
+            Generate Countdown
+        </button>
+    )
+}
+
+export default button;
