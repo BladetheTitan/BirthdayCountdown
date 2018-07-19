e44af317adbdb9e39263ea62f63a0a2bbb37616e
Evan Norland <bladethetitan@gmail.com>
2018-07-19 15:08:52 +0000

    added large text and remaining lebel. finished css front end

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import ChangeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
   
@@ -20,16 +21,17 @@ export default class App extends Component {
       return [
         <Clock/>,
         ChangeDate('Change Date', () => this.setState({ active: false }))
+        LargeText('04/03')
       ]
     } else {
-      return Button('Generate Countdown', () => this.setState({ active: true }))
+      return [
+        <Picker/>,
+        Button('Generate Countdown', () => this.setState({ active: true }))
+      ]
     }
   }.bind(this)
   
   render() {
-    
-    //return <div className=""><Clock/></div>
-    
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
@@ -43,7 +45,6 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
         
-        <Picker/>
         { this.renderItems() }
         
       </div>
