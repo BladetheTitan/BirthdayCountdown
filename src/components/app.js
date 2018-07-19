1e8f2fbb5b1e2189e9ff01757e0faf9e5e6677ae
Evan Norland <bladethetitan@gmail.com>
2018-07-19 15:18:39 +0000

    style fixes

--- src/components/app.js
+++ src/components/app.js
@@ -20,8 +20,9 @@ export default class App extends Component {
     if(this.state.active) {
       return [
         <Clock/>,
-        ChangeDate('Change Date', () => this.setState({ active: false }))
-        LargeText('04/03')
+        ChangeDate('Change Date', () => this.setState({ active: false })),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your 19th birthday</label>
       ]
     } else {
       return [
@@ -36,8 +37,6 @@ export default class App extends Component {
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
       
-      
-        <div className="grid__skew-dark-one-box"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
         
